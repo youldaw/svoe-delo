1878d4bb6055d0545807bb148e354b38f5febd03
youldaw <68648932+youldaw@users.noreply.github.com>
2023-09-26 08:03:20 +0500

    dsfadff
    dsaffxzcv

--- js/script.js
+++ js/script.js
@@ -1,7 +1,48 @@
 $(function () {
-    
-    $('.check-btn input[type="radio"]').on( "change", function() {
+
+    $('.check-btn input[type="radio"]').on("change", function () {
         $(this).parent().parent().siblings('.filter-list').addClass('active');
     });
 
+
+
+    $('.menu-btn').on('click', function (e) {
+        e.preventDefault();
+        let modal3 = $('.modal3')
+        $(modal3).toggleClass('active');
+    });
+
+
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        200: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        400: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        600: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        },
+        900: {
+          slidesPerView: 4,
+          spaceBetween: 30
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 30
+        }
+      }
 });
