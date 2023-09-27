363b82bf56ebd873c3bc1cd39d23f20cbeb74525
youldaw <68648932+youldaw@users.noreply.github.com>
2023-09-28 00:03:17 +0500

    sfsgdsfg
    dfsgdsfgdfg

--- js/script.js
+++ js/script.js
@@ -1,47 +1,92 @@
 $(function () {
 
-    $('.check-btn input[type="radio"]').on("change", function () {
-        $(this).parent().parent().siblings('.filter-list').addClass('active');
-    });
+  $('.check-btn input[type="radio"]').on("change", function () {
+    $(this).parent().parent().siblings('.filter-list').addClass('active');
+  });
 
 
 
-    $('.menu-btn').on('click', function (e) {
-        e.preventDefault();
-        let modal3 = $('.modal3')
-        $(modal3).toggleClass('active');
-    });
+  $('.menu-btn').on('click', function (e) {
+    e.preventDefault();
+    let modal3 = $('.modal3')
+    $(modal3).toggleClass('active');
+  });
+
+  $('.barsToggle').on('click', function (e) {
+    e.preventDefault();
+    let headerMenuLeft = $('.header__menu-media-wrap')
+    $(headerMenuLeft).toggleClass('active');
+  });
+
+  $('.filters-btn').on('click', function (e) {
+    e.preventDefault();
+    let filterModal = $('.filter__modal-wrap')
+    $(filterModal).toggleClass('active');
+  });
+
+
+  $('.sorting-mob').on('click', function (e) {
+    e.preventDefault();
+    let sortingModal = $('.sorting__modal-wrap')
+    $(sortingModal).toggleClass('active');
+  });
+
+
+  $('.header__fixed-search').on('click', function (e) {
+    e.preventDefault();
+    let searchModal = $('.search__modal-wrap')
+    $(searchModal).toggleClass('active');
+    $(this).toggleClass('active')
+  });
+
+
+
+
+  $('.filter__modal-data-link').on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+  
+    if ($('.filter__modal-data-link.active').length > 0) {
+      $('.filter__modal-btn1').addClass('active');
+      $('.filter__modal-btn2').addClass('active');
+    } else {
+      $('.filter__modal-btn1').removeClass('active');
+      $('.filter__modal-btn2').removeClass('active');
+    }
+  });
+  
+
 
 });
 
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 5,
-    spaceBetween: 30,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+  slidesPerView: 5,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    200: {
+      slidesPerView: 2,
+      spaceBetween: 20
     },
-    breakpoints: {
-        200: {
-            slidesPerView: 2,
-            spaceBetween: 20
-        },
-        400: {
-            slidesPerView: 2,
-            spaceBetween: 20
-        },
-        600: {
-          slidesPerView: 3,
-          spaceBetween: 20
-        },
-        900: {
-          slidesPerView: 4,
-          spaceBetween: 30
-        },
-        1200: {
-          slidesPerView: 5,
-          spaceBetween: 30
-        }
-      }
+    400: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    600: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 30
+    }
+  }
 });
